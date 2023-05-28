d2151d14d2fad009ae2aa4aa7846a9ccddaf2555
Agustin Fraile <agustinfraile98@gmail.com>
2023-05-28 02:44:16 -0300

    modificando archivos y agregando estilos

--- src/components/Calculadora/Calculadora.tsx
+++ src/components/Calculadora/Calculadora.tsx
@@ -1,94 +1,22 @@
-import { useState } from 'react';
+import { numeros, useCalculadora } from "./useCalculadora";
 
 import styles from "./Calculadora.module.css";
 
-
 const Calculadora: React.FC = () => {
-  const numeros: any[] = [
-    7,
-    8,
-    9,
-    "DEL",
-    4,
-    5,
-    6,
-    "+",
-    1,
-    2,
-    3,
-    "-",
-    ".",
-    0,
-    "/",
-    "X",
-  ];
-
-  // Valor mostrado en pantalla
-  const [displayValue, setDisplayValue] = useState(''); 
-  // Valor almacenado para la operación
-  const [storedValue, setStoredValue] = useState(''); 
-  // Operador seleccionado
-  const [operator, setOperator] = useState('');
-
-  const calculateResult = () => {
-    const number1 = parseFloat(storedValue);
-    const number2 = parseFloat(displayValue);
-
-    switch (operator) {
-      case '+':
-        return number1 + number2;
-      case '-':
-        return number1 - number2;
-      case '/':
-        return number1 / number2;
-      case 'X':
-        return number1 * number2;
-      default:
-        return '';
-    }
-  };
-
-  const handleClick = (elemento: any) => {
-    if (typeof elemento === 'number' || elemento === '.') {
-      // Si es un número o el punto decimal, agrega el valor al display
-      setDisplayValue(displayValue + elemento);
-    } else if (elemento === 'DEL') {
-      // Si es el botón 'DEL', elimina el último dígito del display
-      setDisplayValue(displayValue.slice(0, -1));
-    } else if (elemento === 'RESET') {
-      // Si es el botón 'RESET', reinicia los valores
-      setDisplayValue('');
-      setStoredValue('');
-      setOperator('');
-    } else if (typeof elemento === 'string' && elemento !== '=') {
-      // Si es un operador (+, -, /, *)
-      setOperator(elemento);
-      setStoredValue(displayValue);
-      setDisplayValue('');
-    } else if (elemento === '=') {
-      // Si es el botón '=', realiza la operación y muestra el resultado
-      const result = calculateResult();
-      console.log(result);
-      setDisplayValue(result.toString());
-      setStoredValue('');
-      setOperator('');
-    }
-  };
-
-
+  const { displayValue, handleClick } = useCalculadora();
 
   return (
     <div className={styles.container_calculadora}>
       <div className={styles.container_calculadora_mostrar}>
-        {displayValue}
+        <h4>{displayValue}</h4>
       </div>
 
       <div className={styles.container_calculadora_numeros}>
         {numeros.map((numero) => (
-          <div 
-            className={styles.numero} key={numero}
-            onClick={()=>handleClick(numero)}    
-        >
+          <div
+            className={styles.numero}
+            key={numero}
+            onClick={() => handleClick(numero)}>
             {numero}
           </div>
         ))}
@@ -99,17 +27,13 @@ const Calculadora: React.FC = () => {
                         ${styles.numero_resultado} 
                         ${styles.resultado_red}
                     `}
-            onClick={()=>handleClick('=')}    
-            
-        >
+          onClick={() => handleClick("=")}>
           =
         </div>
-        <div 
-            className={styles.numero_resultado}
-            onClick={()=>handleClick('RESET')}    
-            
-        >
-            RESET
+        <div
+          className={styles.numero_resultado}
+          onClick={() => handleClick("RESET")}>
+          RESET
         </div>
       </div>
     </div>
---
+++ src/components/Calculadora/useCalculadora.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+export const numeros: any[] = [
+  7,
+  8,
+  9,
+  "DEL",
+  4,
+  5,
+  6,
+  "+",
+  1,
+  2,
+  3,
+  "-",
+  ".",
+  0,
+  "/",
+  "X",
+];
+
+export const useCalculadora = () => {
+  const [displayValue, setDisplayValue] = useState(""); // Valor mostrado en pantalla
+  const [storedValue, setStoredValue] = useState(""); // Valor almacenado para la operación
+  const [operator, setOperator] = useState(""); // Operador seleccionado
+
+  const handleClick = (elemento: any) => {
+    if (typeof elemento === "number" || elemento === ".") {
+      setDisplayValue(displayValue + elemento);
+    } else if (elemento === "DEL") {
+      setDisplayValue(displayValue.slice(0, -1));
+    } else if (elemento === "RESET") {
+      resetearCalculadora();
+    } else if (typeof elemento === "string" && elemento !== "=") {
+      setOperator(elemento);
+      setStoredValue(displayValue);
+      setDisplayValue("");
+    } else if (elemento === "=") {
+      calcularResultado();
+    }
+  };
+
+  const resetearCalculadora = () => {
+    setDisplayValue("");
+    setStoredValue("");
+    setOperator("");
+  };
+
+  const calcularResultado = () => {
+    const number1 = parseFloat(storedValue);
+    const number2 = parseFloat(displayValue);
+
+    let result = "";
+
+    switch (operator) {
+      case "+":
+        result = (number1 + number2).toString();
+        break;
+      case "-":
+        result = (number1 - number2).toString();
+        break;
+      case "/":
+        result = (number1 / number2).toString();
+        break;
+      case "X":
+        result = (number1 * number2).toString();
+        break;
+      default:
+        result = "";
+    }
+
+    setDisplayValue(result);
+    setStoredValue("");
+    setOperator("");
+  };
+
+  return { displayValue, handleClick };
+};
